a1c63b9c7dca3441faa1fafb0667455df9b4a3ba
darkerbit <mail.darkerbit@gmail.com>
2023-03-26 17:36:43 +0300

    Move items to vanilla Redstone tab

--- src/main/java/io/github/darkerbit/redstonerelays/item/Items.java
+++ src/main/java/io/github/darkerbit/redstonerelays/item/Items.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,10 +15,6 @@ import net.minecraft.registry.tag.TagKey;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public final class Items {
-    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(RedstoneRelays.identifier("group"))
-            .icon(() -> new ItemStack(Blocks.TOGGLE_RELAY))
-            .build();
-
     public static final TagKey<Item> UPGRADE_TAG = TagKey.of(RegistryKeys.ITEM, RedstoneRelays.identifier("upgrade_module"));
 
     public static Item RANGE_UPGRADE = new Item(new QuiltItemSettings().maxCount(1));
@@ -27,7 +24,7 @@ public final class Items {
         Registry.register(Registries.ITEM, RedstoneRelays.identifier("range_upgrade"), RANGE_UPGRADE);
         Registry.register(Registries.ITEM, RedstoneRelays.identifier("pulse_upgrade"), PULSE_UPGRADE);
 
-        ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP).register(content -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE_BLOCKS).register(content -> {
             content.addItem(Blocks.PUSH_RELAY);
             content.addItem(Blocks.TOGGLE_RELAY);
             content.addItem(Blocks.PULSE_RELAY);
